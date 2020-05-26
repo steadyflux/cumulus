7e26106c65ba7d00094713a0c3598b3a2f22834b
Mark Boyd <markdavidboyd@gmail.com>
2020-05-26 15:29:11 -0400

    update tests for applyWorkflow

--- packages/api/tests/models/granules/test-granule-applyWorkflow.js
+++ packages/api/tests/models/granules/test-granule-applyWorkflow.js
@@ -11,25 +11,70 @@ const { constructCollectionId } = require('@cumulus/message/Collections');
 const Granule = require('../../../models/granules');
 const Rule = require('../../../models/rules');
 
-let lambdaStub;
 test.before(async () => {
   process.env.GranulesTable = randomString();
   await new Granule().createTable();
-
-  lambdaStub = sinon.stub(Lambda, 'invoke').resolves(true);
 });
 
-test('applyWorkflow accepts list of granules', async (t) => {
+test.beforeEach((t) => {
+  t.context.lambdaStub = sinon.stub(Lambda, 'invoke').resolves(true);
+});
+
+test.afterEach.always((t) => {
+  t.context.lambdaStub.restore();
+});
+
+test.serial('applyWorkflow accepts list of granules', async (t) => {
+  const { lambdaStub } = t.context;
   const granulesModel = new Granule();
-  // TODO: remove stub and use spy so we can inspect args
-  const stub = sinon.stub(Rule, 'buildPayload').resolves({});
+  const stub = sinon.stub(Rule, 'buildPayload').callsFake(
+    (payload) => Promise.resolve(payload)
+  );
   t.teardown(() => stub.restore());
+  const granules = [{
+    collectionId: constructCollectionId('test', '001'),
+    provider: 'test-provider'
+  }];
   await granulesModel.applyWorkflow(
-    [{
-      collectionId: constructCollectionId('test', '001'),
-      provider: 'test-provider'
-    }],
+    granules,
     'test-workflow'
   );
   t.is(lambdaStub.callCount, 1);
+  t.deepEqual(lambdaStub.firstCall.lastArg.payload.granules, granules);
+});
+
+test.serial('applyWorkflow correctly maps input granules based on collection/provider', async (t) => {
+  const { lambdaStub } = t.context;
+  const granulesModel = new Granule();
+  const stub = sinon.stub(Rule, 'buildPayload').callsFake(
+    (payload) => Promise.resolve(payload)
+  );
+  t.teardown(() => stub.restore());
+
+  const collection1 = constructCollectionId('foo', '001');
+  const collection2 = constructCollectionId('bar', '001');
+  const provider1 = 'test-provider';
+  const provider2 = 'test2-provider';
+
+  const granules = [{
+    collectionId: collection1,
+    provider: provider1
+  }, {
+    collectionId: collection1,
+    provider: provider1
+  }, {
+    collectionId: collection1,
+    provider: provider2
+  }, {
+    collectionId: collection2,
+    provider: provider2
+  }];
+  await granulesModel.applyWorkflow(
+    granules,
+    'test-workflow'
+  );
+  t.is(lambdaStub.callCount, 3);
+  t.deepEqual(lambdaStub.args[0][1].payload.granules, [granules[0], granules[1]]);
+  t.deepEqual(lambdaStub.args[1][1].payload.granules, [granules[2]]);
+  t.deepEqual(lambdaStub.args[2][1].payload.granules, [granules[3]]);
 });
